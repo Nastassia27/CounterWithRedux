7ad787bf45d1bb2011ead0b4da15caf0fca3b765
Nastassia27 <anastasia.derugo@gmail.com>
2023-08-08 17:48:07 +0300

    add useDispatch and useSelector

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -3,7 +3,7 @@ import s from "./counter.module.css";
 import {Button} from "./Button";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../redux/store";
-import {CounterType} from "./counter";
+import {changeCounterAC, resetCounterAC} from "../redux/counter-reducer";
 
 /*type PropsType = {
     counter: number
@@ -15,31 +15,56 @@ import {CounterType} from "./counter";
     previousMaxValue: number
     previousMinValue: number
 }*/
+
+export type CounterType = {
+    maxValue: number,
+    minValue: number,
+    counter: number,
+    buttonClicked: boolean,
+    previousMinValue: number,
+    previousMaxValue: number,
+}
 export const MainWindow = () => {
     let dispatch = useDispatch()
-    let counter = useSelector<AppRootStateType, CounterType>(state=> state.counter)
-const labelVariant = ()=>{
-    if(!counter.buttonClicked && (props.minValue < 0 || props.maxValue < 0 || props.maxValue <= props.minValue)){
-        return <label className={s.incorrectNumber} htmlFor="counter">Incorrect value</label>
-    } else if (!props.buttonClicked){
-        return  <label className={s.firstMessage} htmlFor="counter">Enter values and press "Set"</label>
-    } else{
-        return <p>{props.counter}</p>
+    let {
+        minValue,
+        maxValue,
+        counter,
+        buttonClicked,
+        previousMinValue,
+        previousMaxValue,
+    } = useSelector((state: AppRootStateType) => state.counterer)
+
+
+    const labelVariant = () => {
+        if (!buttonClicked && (minValue < 0 || maxValue < 0 || maxValue <= minValue)) {
+            return <label className={s.incorrectNumber} htmlFor="counter">Incorrect value</label>
+        } else if (!buttonClicked) {
+            return <label className={s.firstMessage} htmlFor="counter">Enter values and press "Set"</label>
+        } else {
+            return <div>{counter}</div>
+        }
     }
 
-}
+    const changeCounter = () => {
+      //  debugger
+        dispatch(changeCounterAC())
+    }
+    const resetCounter = () => {
+        dispatch(resetCounterAC())
+    }
 
     return (
 
         <div className={s.counter}>
-            <div className={props.counter === props.previousMaxValue ? s.numberError : s.number}>
-                { labelVariant()}
+            <div className={counter === previousMaxValue ? s.numberError : s.number}>
+               {labelVariant()}
             </div>
             <div className={s.buttons}>
-                <Button className={s.click} title={'inc'} callBack={props.changeCounter}
-                        disabled={props.counter === props.previousMaxValue || !props.buttonClicked || props.minValue < 0 || props.maxValue < 0 || props.maxValue <= props.minValue}/>
-                <Button className={s.click} title={'reset'} callBack={props.resetCounter}
-                        disabled={props.counter === props.previousMinValue || !props.buttonClicked}/>
+                <Button className={s.click} title={'inc'} callBack={changeCounter}
+                        disabled={counter === previousMaxValue || !buttonClicked || minValue < 0 || maxValue < 0 || maxValue <= minValue}/>
+                <Button className={s.click} title={'reset'} callBack={resetCounter}
+                        disabled={counter === previousMinValue || !buttonClicked}/>
             </div>
 
         </div>
--- src/components/SetValues.tsx
+++ src/components/SetValues.tsx
@@ -2,6 +2,10 @@ import React, {ChangeEvent, useState} from 'react';
 import s from "./counter.module.css";
 import {Button} from "./Button";
 import Input from "./Input";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../redux/store";
+import {CounterType} from "./MainWindow";
+import {onChangeMaxAC, onChangeMinAC, setValueAC} from "../redux/counter-reducer";
 
 /*
 type PropsTypes = {
@@ -14,20 +18,39 @@ type PropsTypes = {
 */
 
 export const SetValues = () => {
+    let dispatch = useDispatch()
+    let {
+        minValue,
+        maxValue,
+        ...restProps
+    } = useSelector((state: AppRootStateType) => state.counterer)
 
 
+    const changeMax = (event: ChangeEvent<HTMLInputElement>) => {
+        dispatch(onChangeMaxAC(event))
+    }
+    const changeMin = (event: ChangeEvent<HTMLInputElement>) => {
+        dispatch(onChangeMinAC(event))
+    }
+    const setValue = () => {
+        dispatch(setValueAC())
+
+    }
     return (
         <div className={s.counter}>
             <div className={s.inputgroup}>
                 <label htmlFor="maxValue">Max Value</label>
-                <Input class={props.maxValue<0 || props.maxValue<=props.minValue ? s.inputError: ''} type={'number'} id={'maxValue'} value={props.maxValue} callback={props.changeMax}/>
+                <Input class={maxValue < 0 || maxValue <= minValue ? s.inputError : ''} type={'number'} id={'maxValue'}
+                       value={maxValue} callback={changeMax}/>
             </div>
             <div className={s.inputgroup}>
                 <label htmlFor="minValue">Min Value</label>
-                <Input class={props.minValue<0 || props.maxValue<=props.minValue ? s.inputError: ''} type={'number'} id={'minValue'} value={props.minValue} callback={props.changeMin}/>
+                <Input class={minValue < 0 || maxValue <= minValue ? s.inputError : ''} type={'number'} id={'minValue'}
+                       value={minValue} callback={changeMin}/>
             </div>
             <div className={s.buttons}>
-                <Button disabled={props.minValue<0 || props.maxValue<0 || props.maxValue<=props.minValue} className={s.click} title={'Set'} callBack={props.setValue}/>
+                <Button disabled={minValue < 0 || maxValue < 0 || maxValue <= minValue} className={s.click}
+                        title={'Set'} callBack={setValue}/>
             </div>
         </div>
     );
