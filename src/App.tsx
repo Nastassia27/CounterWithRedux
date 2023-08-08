2428545d15cca124dee5d9bbde842c046ff32bf2
Nastassia27 <anastasia.derugo@gmail.com>
2023-08-08 17:49:08 +0300

    place all components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
+import s from './components/counter.module.css'
 import './App.css';
-import {Counter} from "./components/counter";
+import MainWindow from "./components/MainWindow";
+import SetValues from "./components/SetValues";
 
 function App() {
 
     return (
-        <div>
-            <Counter/>
+        <div className={s.wrapper}>
+            <div>
+                <SetValues/>
+
+            </div>
+            <div>
+                <MainWindow/>
+            </div>
+            {/*<Counter/>*/}
         </div>
-    );
+    )
 }
+
 
 export default App;
